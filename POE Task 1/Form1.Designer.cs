d32d5cb195e19122d50cfc28ccd2f6e80c36278e
VinnieRepo <91118842+VinnieRepo@users.noreply.github.com>
2021-11-03 20:06:02 +0200

    Add project files.

---
+++ POE Task 1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace POE_Task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MapLabel = new System.Windows.Forms.Label();
+            this.CharacterLabel = new System.Windows.Forms.Label();
+            this.EnemyLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MapLabel
+            // 
+            this.MapLabel.AutoSize = true;
+            this.MapLabel.Location = new System.Drawing.Point(72, 54);
+            this.MapLabel.Name = "MapLabel";
+            this.MapLabel.Size = new System.Drawing.Size(50, 20);
+            this.MapLabel.TabIndex = 0;
+            this.MapLabel.Text = "label1";
+            // 
+            // CharacterLabel
+            // 
+            this.CharacterLabel.AutoSize = true;
+            this.CharacterLabel.Location = new System.Drawing.Point(375, 81);
+            this.CharacterLabel.Name = "CharacterLabel";
+            this.CharacterLabel.Size = new System.Drawing.Size(50, 20);
+            this.CharacterLabel.TabIndex = 1;
+            this.CharacterLabel.Text = "label2";
+            // 
+            // EnemyLabel
+            // 
+            this.EnemyLabel.AutoSize = true;
+            this.EnemyLabel.Location = new System.Drawing.Point(375, 182);
+            this.EnemyLabel.Name = "EnemyLabel";
+            this.EnemyLabel.Size = new System.Drawing.Size(50, 20);
+            this.EnemyLabel.TabIndex = 2;
+            this.EnemyLabel.Text = "label3";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(57, 353);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(165, 66);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click_1);
+            // 
+            // UpButton
+            // 
+            this.UpButton.Location = new System.Drawing.Point(582, 297);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(94, 29);
+            this.UpButton.TabIndex = 4;
+            this.UpButton.Text = "Up";
+            this.UpButton.UseVisualStyleBackColor = true;
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.Location = new System.Drawing.Point(479, 344);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(94, 29);
+            this.LeftButton.TabIndex = 5;
+            this.LeftButton.Text = "Left";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            // 
+            // RightButton
+            // 
+            this.RightButton.Location = new System.Drawing.Point(679, 344);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(94, 29);
+            this.RightButton.TabIndex = 6;
+            this.RightButton.Text = "Right";
+            this.RightButton.UseVisualStyleBackColor = true;
+            // 
+            // DownButton
+            // 
+            this.DownButton.Location = new System.Drawing.Point(582, 390);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(94, 29);
+            this.DownButton.TabIndex = 7;
+            this.DownButton.Text = "Down";
+            this.DownButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.LeftButton);
+            this.Controls.Add(this.UpButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.EnemyLabel);
+            this.Controls.Add(this.CharacterLabel);
+            this.Controls.Add(this.MapLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label CharacterLabel;
+        private System.Windows.Forms.Label EnemyLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Label MapLabel;
+    }
+}
+
---
+++ POE Task 1/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POE_Task_1
+{
+   
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //Base Class for Tiles
+        public abstract class tile
+
+
+        {
+            protected int X;
+            public int x
+            {
+                get { return X; }
+                set { X = value; }
+            }
+            public int Y;
+            public int y
+            {
+                get { return Y; }
+                set { Y = value; }
+            }
+            
+            
+            public void valuex(int value)
+            {
+                x = value;
+            }
+            public void valuey(int value)
+            {
+                y = value;
+            }
+            public string tilesymbol;
+
+            public string symbolval
+            {
+                get { return symbolval; }
+                set { symbolval = value; }
+
+            }
+
+            //Tiletype and Enumerator
+
+
+            public enum Tiletypes
+            {
+                Hero = 1,
+                Enemy = 2,
+                Gold = 3,
+                Weapon =4,
+            }
+
+
+            // Default values class
+            public class defaults : tile
+            {
+                public void Basevalues(int xval, int yval, int tiletype)
+                {
+                    x = xval;
+                    y = yval;
+                    var Tiletype = (Tiletypes)tiletype;
+
+
+
+                }
+
+            }
+            //Obstacles Classs
+            public class obstacles : defaults
+            {
+                public void Places(int xval, int yval, int tiletype)
+                {
+                    Basevalues(xval, yval, tiletype);
+                }
+
+
+
+            }
+            // Empty Tiles class.
+            public class emptyTiles : defaults
+            {
+                public void Places(int xval, int yval, int tiletype)
+                {
+                    Basevalues(xval, yval, tiletype);
+                }
+
+
+            }
+            // Base class for characters
+            public abstract class Character : tile
+            {
+                protected int HP;
+                public int hp
+                {
+                    get { return HP; }
+                    set { HP = value; }
+                }
+                protected int MAXHP;
+                public int maxhp
+                {
+                    get { return MAXHP; }
+                    set { MAXHP = value; }
+                }
+                protected int Damage;
+                public int damage
+                {
+                    get { return Damage; }
+                    set { Damage = value; }
+                }
+
+
+                private List<tile> vision;
+                public List<tile> Vision
+                {
+                    get { return vision; }
+                    set { vision = value; }
+                }
+                public enum Movement
+                { Up, Down, Left, Right, NoMovement };
+
+                //Position of the character.
+                protected void position(int tilex, int tiley)
+                {
+                    x = tilex;
+                    y = tiley;
+                }
+                // Death check.
+                public bool isdead(int value)
+                {
+                    if (value == 1)
+                    {
+                        return true;
+                    }
+
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+                //Attack method to be filled in later.
+                public virtual void attack(Character Target)
+                {
+                    Target.HP -= Damage;
+                }
+                //Distance Calc
+                private int distanceto(int target, int charpos)
+                {
+                    int distance;
+                    distance = target - charpos;
+                    return distance;
+
+                }
+                //Range check
+                private bool CheckRange(int target, int charpos)
+                {
+                    int distance = distanceto(target, charpos);
+                    if (distance <= 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                //movement using enum
+                public void CharacterMove(int direction, int moveamount)
+                {
+                    var whichway = (Movement)direction;
+                    switch (whichway)
+                    {
+                        case Movement.Up:
+                            y = y - 1;
+                            break;
+                        case Movement.Down:
+                            y = y + 1;
+                            break;
+                        case Movement.Left:
+                            x = x - 1;
+                            break;
+                        case Movement.Right:
+                            x = x + 1;
+                            break;
+
+                    }
+
+                }
+                //return move to be overridden
+                public abstract Movement returnmove(Movement move = 0);
+
+
+                //Tostring to be overridden.
+                public abstract override string ToString();
+            }
+            //Enemy Class
+            public abstract class Enemy : Character
+            {
+                //Enemy Constructor
+                public void EnemyStats(int posy, int posx, int StartHP, string symbol, int attack)
+                {
+                    x = posx;
+                    y = posy;
+                    HP = StartHP;
+                    MAXHP = StartHP;
+                    Damage = attack;
+
+                }
+                //Overridden String
+                public override string ToString()
+                {
+                    string Stats = GetType().Name + "\n";
+                    Stats += "at [" + x.ToString() + "," + y.ToString() + "] \n";
+                    Stats += HP.ToString() + "HP \n;";
+                    return "EnemyClassName at " + "[" + x + ',' + y + "]" + "(" + Damage + ")";
+                }
+
+
+            }
+            // Goblin Subclass
+            public class Goblin : Enemy
+            {    //Constructor
+                public void GoblinStats(int tilex, int tiley)
+                {
+                    x = x;
+                    y = y;
+                    HP = 10;
+                    Damage = 1;
+
+
+                }
+                //random movement
+                public override Movement returnmove(Movement move = Movement.Up)
+                {
+                    Random r = new Random();
+                    int num = r.Next(4);
+                    return (Movement)num;
+                }
+            }
+            //Hero Subclass
+            public class Hero : Character
+            {
+                public void HeroStats(int x, int y, int hp)
+                {
+                    position(x, y);
+                    HP = hp;
+                    Damage = 2;
+
+                }
+                //Movement
+                public override Movement returnmove(Movement move = Movement.Up)
+                {
+                    return (Movement)move;
+                }
+                //Tostring overrider
+                public override string ToString()
+                {
+                    return "Player Stats \r\n"
+                        + "HP: " + HP + "/" + MAXHP + "\r\n" + "Damage:" + "(" + Damage + ")\r\n" + "[" + x + ',' + y + "]";
+                }
+            }
+            //Attempt at the map creating class, this is where i hit my snag.
+            public class Maphelp
+            {
+
+
+
+                char HeroIcon = '@';
+                private char[,] Enemy { get; set; }
+                public int mapwidth { get; set; }
+                public int mapheight { get; set; }
+
+                public char[,] maptiles;
+
+                char[,] enemyArray { get; set; }
+
+                Random mappy = new Random();
+
+                // Mapfiller
+                public char[,] mapmaking(int maxwidth, int minwidth, int minheight, int maxheight, int numberofenemies)
+                {
+
+
+                    char[,] maptiles = new char[mapwidth, mapheight];
+
+                    for (int i = 0; i < mapwidth; i++)
+                    {
+                        for (int j = 0; j < mapheight; j++)
+                        {
+                            maptiles[i, j] = 'v';
+                        }
+
+
+
+
+
+
+
+                    }
+                    
+                    
+                    return maptiles;
+                }
+                //Map Populator
+                public void create(int Enemynumb)
+                {
+                    int charposy = mappy.Next(1, mapwidth);
+                    int charposx = mappy.Next(1, mapheight);
+                    maptiles[charposx, charposy] = '@';
+
+                    for (int i = 1; i < Enemynumb; i++)
+                    {
+                        
+                        for (int j = 1; j < Enemynumb; j++)
+                        {
+                            int enemyposy = mappy.Next(1, mapheight );
+                            int enemyposx = mappy.Next(1, mapwidth );
+                            if (maptiles[enemyposx, enemyposy] == 'v')
+                            {
+                                maptiles[enemyposy, enemyposx] = '#';
+                            } 
+
+                            else
+                            {
+                                i = i - 1;
+                              
+                            }
+
+                        }
+
+
+
+                    }
+
+                }
+                //gold and weapons
+                public void createtiles(int gold, int weapon)
+
+                {
+                    for (int i = 0; i < gold; i++)
+                    {
+                        for (int j = 0; j < gold; j++)
+                        {
+                            int charposy = mappy.Next(1, mapheight);
+                            int charposx = mappy.Next(1, mapwidth);
+                            if (maptiles[charposx, charposy] == 'v')
+                            {
+                                maptiles[charposx, charposy] = 'G';
+
+                            }
+                            else
+                            {
+                                i = i - 1;
+                            }
+
+                        }
+
+
+                    }
+                    for (int i = 0; i < weapon; i++)
+                    {
+                        for (int j = 0; j < weapon; j++)
+                        {
+                            int charposy = mappy.Next(1, mapheight);
+                            int charposx = mappy.Next(1, mapwidth);
+                            if (maptiles[charposx, charposy] == 'v')
+                            {
+                                maptiles[charposx, charposy] = 'W';
+
+                            }
+                            else
+                            {
+                                i = i - 1;
+                            }
+                        }
+
+
+                    }
+                }
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+        //Instead of having GameEngine, my code runs off the startbutton.
+       
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click_1(object sender, EventArgs e)
+        {
+            tile.Maphelp make = new tile.Maphelp();
+            Random mappy = new Random();
+            int elements = 2;
+            int minw = 5;
+            int maxw = mappy.Next(minw, 20);
+            int minh = 5;
+            int maxh = mappy.Next(minh, 20);
+            make.mapheight = mappy.Next(minh, maxh);
+            make.mapwidth = mappy.Next(minw, maxw);
+
+
+            make.maptiles = make.mapmaking(maxw, minw, minh, maxh, elements);
+            make.create(elements);
+            make.createtiles(elements, elements);
+
+            string mapString = "";
+            for (int i = 0; i < make.maptiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < make.maptiles.GetLength(1); j++)
+                {
+                    mapString += make.maptiles[i, j].ToString();
+                    mapString += " ";
+                }
+
+                mapString += Environment.NewLine;
+            }
+            this.MapLabel.Text = mapString;
+            tile.Character.Hero argumentpass = new tile.Character.Hero();
+            tile.Character.Goblin argumentpass2 = new tile.Character.Goblin();
+            this.CharacterLabel.Text = argumentpass.ToString();
+            this.EnemyLabel.Text = argumentpass2.ToString();
+
+
+        }
+    }
+}
